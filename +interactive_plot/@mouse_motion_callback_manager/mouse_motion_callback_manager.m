fbc76f59fdacc00e39e24679b80b385cd7113c71
Greg <ghg3@duke.edu>
2017-11-10 23:40:05 -0500

    First try at scroll bar
    Partial functionality, but still pretty buggy. Having trouble with action listener to try to keep track of zoom.
    
    Scroll bar also doesn't quite match up, also expands sometimes...

--- +interactive_plot/@mouse_motion_callback_manager/mouse_motion_callback_manager.m
+++ +interactive_plot/@mouse_motion_callback_manager/mouse_motion_callback_manager.m
@@ -25,7 +25,20 @@ classdef mouse_motion_callback_manager < handle
             
             obj.parent.line_moving_processor.resizePlots();
         end
-        
+        function initializeScrolling(obj)
+            % temporary hack. this is not efficient
+            %--
+            cur_mouse_coords = get(obj.fig_handle, 'CurrentPoint');
+            cur_mouse_x = cur_mouse_coords(1);
+            obj.parent.scroll_bar.prev_mouse_x = cur_mouse_x;
+            %--
+            set(obj.fig_handle, 'WindowButtonMotionFcn', @(~,~) obj.parent.scroll_bar.scroll());
+            set(obj.fig_handle, 'WindowButtonUpFcn', @(~,~) obj.releaseScrollBar());   
+        end
+        function releaseScrollBar(obj)
+            set(obj.fig_handle, 'WindowButtonMotionFcn', '');
+            obj.parent.scroll_bar.updateAxes();
+        end
     end
 end
 
--- +interactive_plot/@scroll_bar/scroll_bar.m
+++ +interactive_plot/@scroll_bar/scroll_bar.m
@@ -10,15 +10,21 @@ classdef scroll_bar <handle
         left_button
         right_button
         background_bar
-        movable_bar
+        slider
         
+        base_y = 0.01;
         left_limit
         right_limit
-        bar_height = 0.05;
+        bar_height = 0.04;
+        width
         
         total_time_range
         time_range_in_view
         
+        slider_left_x
+        slider_right_x
+        
+        prev_mouse_x
     end
     
     methods
@@ -32,14 +38,70 @@ classdef scroll_bar <handle
         obj.left_limit = temp2(1);
         obj.right_limit = temp2(1) + temp2(3);
         width = obj.right_limit - obj.left_limit;
-        obj.background_bar = annotation('rectangle', [obj.left_limit, 0, width, obj.bar_height], 'FaceColor', 'w');
+        obj.background_bar = annotation('rectangle', [obj.left_limit, obj.base_y, width, obj.bar_height], 'FaceColor', 'w');
         
         
         data_objs =  get(temp1, 'Children');
         time_vector = data_objs.XData;
         obj.total_time_range = max(time_vector) - min(time_vector);
         
-        end
+        %create the slider
+        obj.slider = annotation('rectangle', [obj.left_limit, obj.base_y, width, obj.bar_height], 'FaceColor', 'k');
+        obj.slider_left_x = obj.left_limit;
+        obj.slider_right_x = obj.right_limit;
         
+        % get the time range that we have zoomed to (currently just base it
+        % on the first plot -- will need to figure out how to update all
+        % plots zooming together
+        set(obj.slider, 'ButtonDownFcn', @(~,~) obj.parent.mouse_manager.initializeScrolling);
+        ax = obj.parent.axes_handles{1};
+        %addlistener(ax, 'XLim', 'PostSet', @(~,~) obj.checkTimeRange);
+        end
+        function checkTimeRange(obj)
+           % just check axes 1 for proof of concept...
+           ax = obj.parent.axes_handles{1};
+           x_min = ax.XLim(1);
+           x_max = ax.XLim(2);
+           
+           obj.slider_left_x = x_min/obj.total_time_range;
+           obj.slider_right_x = x_max/obj.total_time_range;
+           obj.width = obj.slider_right_x - obj.slider_left_x;
+           obj.time_range_in_view = ax.XLim;
+           
+           % put the 
+           set(obj.slider, 'Position', [obj.slider_left_x, obj.base_y, obj.width, obj.bar_height]);
+        end
+        function scroll(obj)
+            cur_mouse_coords = get(obj.fig_handle, 'CurrentPoint');
+            cur_mouse_x = cur_mouse_coords(1);
+            
+            dif = cur_mouse_x - obj.prev_mouse_x;
+            new_right_limit = obj.slider_right_x  + dif;
+            new_left_limit = obj.slider_left_x + dif;
+            
+            if new_right_limit >= obj.right_limit
+                % have to base position on this edge
+                obj.slider_left_x = obj.right_limit - obj.width;
+                obj.slider_right_x = obj.right_limit;
+            elseif new_left_limit <= obj.left_limit
+                obj.slider_left_x = obj.left_limit;
+                obj.slider_right_x = obj.slider_left_x + obj.width;
+            else
+                obj.slider_left_x = new_left_limit;
+                obj.slider_right_x = new_right_limit;
+            end
+            set(obj.slider, 'Position', [obj.slider_left_x, obj.base_y, obj.width, obj.bar_height]);
+            obj.prev_mouse_x = cur_mouse_x;
+        end
+        function updateAxes(obj)
+            % convert the left position to a time
+            max_width = (obj.right_limit - obj.left_limit);
+            left_time = (obj.slider_left_x/max_width)*obj.total_time_range;
+            right_time = (obj.slider_right_x/max_width)*obj.total_time_range;
+            
+            axes_handles = obj.parent.axes_handles;
+            ax = axes_handles{1};
+            ax.XLim = [left_time, right_time];
+        end
     end
 end
