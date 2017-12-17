159769199a51f519b2d3e9c22aa14f20191994c9
Jim Hokanson <jim.hokanson@gmail.com>
2017-12-16 20:04:45 -0500

    Finished basic comments functionality

--- +interactive_plot/+utils/getCurrentMousePoint.m
+++ +interactive_plot/+utils/getCurrentMousePoint.m
@@ -1,7 +1,23 @@
-function [x,y] = getCurrentMousePoint(fig_handle)
+function [x,y] = getCurrentMousePoint(fig_handle,varargin)
 %
-%   [x,y] = interactive_plot.utils.getCurrentMousePoint(fig_handle)
+%   [x,y] = interactive_plot.utils.getCurrentMousePoint(fig_handle,varargin)
+%
+%   Optional Inputs
+%   ---------------
+%   pixels : default false
+
+%We currently assume that we are working with normalized units
+
+in.pixels = false;
+in = interactive_plot.sl.in.processVarargin(in,varargin);
+
 cur_mouse_coords = get(fig_handle, 'CurrentPoint');
 y = cur_mouse_coords(2);
 x = cur_mouse_coords(1);
+if in.pixels
+    p = getpixelposition(fig_handle);
+    y = p(4)*y;
+    x = p(3)*x;
+end
+
 end
--- +interactive_plot/@comments/comments.m
+++ +interactive_plot/@comments/comments.m
@@ -12,10 +12,14 @@ classdef comments < handle
     
     %{
     
+    - when many comments in local area, allow selection in the
+    uicontextmenu -??? How to get back to main selection????? - nested
+     select then action?
+    - in menu, add option for moving taht follows mouse and on mouse
+      down dows the releasing
     - DONE move comment
-    - delete comment
-    
-    - edit comment
+    - DONE delete comment
+    - DONE edit comment
     - DONE ylim text adjust
     - DONE no string no comment
     - DONE hit return to add comment
@@ -25,21 +29,26 @@ classdef comments < handle
     properties
         fig_handle
         axes_handles
+        mouse_man
         
         h_lines %One line handle per axes
         
         bottom_axes %Handle to the axes on the bottom (for text placement)
+        
+        %These are currently inaccurate due to resizing ...
         y_top_axes
         y_bottom_axes
         
-        mouse_man
-                
+        %First menu item - for showing the name of the selected label
+        h_m1
+        h_menu
+        
         %Comment Data
         %--------------
         strings
         times
         h_text
-        is_valid %NYI, when a comment is deleted, mark it as invalid
+        is_deleted
         
         n_comments = 0
         
@@ -67,9 +76,22 @@ classdef comments < handle
             obj.strings = cell(1,100);
             obj.h_text = cell(1,100);
             obj.times = zeros(1,100);
+            obj.is_deleted = false(1,100);
             
+            %This is needed to move the text to the botom of the bottom
+            %axes
             obj.ylim_listener = addlistener(obj.bottom_axes, 'YLim', ...
                 'PostSet', @(~,~) obj.ylimChanged);
+            
+            
+            c = uicontextmenu;
+            
+            obj.h_m1 = uimenu(c,'label','');
+            uimenu(c,'Label','delete comment','Separator','on',...
+                'Callback',@(~,~)obj.deleteComment);
+            uimenu(c,'Label','edit comment','Callback',@(~,~)obj.editComment);
+            
+            obj.h_menu = c;
             
             n_axes = length(obj.axes_handles);
             temp = cell(1,n_axes);
@@ -81,9 +103,77 @@ classdef comments < handle
             
             obj.h_lines = temp;
         end
-        function commentSelected(obj,I)
+        function deleteComment(obj)
+            I = obj.selected_line_I;
+            obj.is_deleted(I) = true;
+            set(obj.h_text{I},'Visible','off')
+            obj.renderComments();
+        end
+        function editComment(obj)
+            I = obj.selected_line_I;
+            options.Resize='on';
+            options.WindowStyle='normal';
+            %options.Interpreter='tex';
             
+            %Must be a cell array
+            default_answer = obj.strings(I);
+            answer = inputdlg('Enter new value','Edit Comment',1,default_answer,options);
+            if ~isempty(answer)
+                new_string = answer{1};
+                obj.strings{I} = new_string;
+                display_string = h__getDisplayString(new_string,I);
+                set(obj.h_text{I},'String',display_string)
+            end
+            
+            
+        end
+        function rightClickComment(obj)
+            %
+            %
+            %   When right clicking, show the uicontextmenu
+            %
+            %   Format:
+            %   1 - show the selected string
+            %   2... - show the action options
+            
+            [x,y] = interactive_plot.utils.getCurrentMousePoint(...
+                obj.fig_handle,'pixels',true);
+            
+            I = obj.selected_line_I;
+            string = obj.strings{I};
+            display_string = h__getDisplayString(string,I);
+            if length(display_string) > 20
+                display_string = display_string(1:20);
+                display_string(18:20) = '...';
+            end
+            
+            drawnow();
+            set(obj.h_m1,'Label',display_string);
+            set(obj.h_menu,'Position',[x y],'Visible','on')
+            drawnow();
+        end
+        function commentSelected(obj,I)
+            %
+            %   Mouse clicked on a comment ...
+            %
+            %   Funtionality
+            %   ------------
+            %   left click - drag to move
+            %   right click - menu option
+            
+            %Set internal state ----------
             obj.selected_line_I = I;
+            
+            %Look for right-click
+            %- Unfortunately, the right click seems to not be specific
+            %- https://www.mathworks.com/help/releases/R2014a/matlab/ref/figure_props.html#SelectionType
+            if strcmp(get(obj.fig_handle,'SelectionType'),'alt')
+                obj.rightClickComment();
+                return
+            end
+            
+            %On clicking, create a line that represents the location of the
+            %comment
             
             x = interactive_plot.utils.getCurrentMousePoint(obj.fig_handle);
             y_line = [obj.y_bottom_axes obj.y_top_axes];
@@ -93,12 +183,17 @@ classdef comments < handle
             obj.mouse_man.setMouseUpFunction(@obj.mouseUp);
         end
         function movingComment(obj)
+            %
+            %   mouse moved, update position of the comment line
             h = obj.h_text{obj.selected_line_I};
             set(h,'Color','r')
             x = interactive_plot.utils.getCurrentMousePoint(obj.fig_handle);
             set(obj.h_line_temp,'X',[x x]);
         end
         function mouseUp(obj)
+            %
+            %   mouse released, move comment to location of the mouse
+            
             h = obj.h_text{obj.selected_line_I};
             
             %Update time based on mouse
@@ -106,7 +201,7 @@ classdef comments < handle
             x_line = get(obj.h_line_temp,'X');
             new_time = interactive_plot.utils.translateXFromFigToAxes(...
                 obj.bottom_axes,x_line(1));
-                    
+            
             obj.times(obj.selected_line_I) = new_time;
             
             %Move tet and reset text color
@@ -116,7 +211,7 @@ classdef comments < handle
             
             delete(obj.h_line_temp);
             
-            obj.mouse_man.initDefaultState();    
+            obj.mouse_man.initDefaultState();
             
             obj.renderComments();
         end
@@ -125,6 +220,12 @@ classdef comments < handle
             %   - intended for loading from disk
         end
         function addComment(obj,time,str)
+            %
+            %   addComment(obj,time,str)
+            %
+            %   Add comment to figure. Currently exposed in GUI via
+            %   top_panel.
+            
             if isempty(str)
                 return
             end
@@ -132,13 +233,13 @@ classdef comments < handle
             I = obj.n_comments + 1;
             
             if I > length(obj.times)
-            	%TODO: Handle overflow resizing - both original and sorted arrays
+                %TODO: Handle overflow resizing
             end
             
             obj.strings{I} = str;
             obj.times(I) = time;
             ylim = get(obj.bottom_axes,'YLim');
-            display_string = sprintf(' %d) %s',I,str);
+            display_string = h__getDisplayString(str,I);
             obj.h_text{I} = text(obj.bottom_axes,time,ylim(1),display_string,...
                 'Rotation',90,'UserData',I,'BackgroundColor',[1 1 1 0.2],...
                 'ButtonDownFcn',@(~,~)obj.commentSelected(I));
@@ -158,6 +259,9 @@ classdef comments < handle
                 %Line Editing
                 %-------------------------------------------
                 x = obj.times(1:obj.n_comments);
+                %In this way we can track the values, i.e. we
+                %never really delete ...
+                x(obj.is_deleted(1:obj.n_comments)) = NaN;
                 x_line = NaN(1,3*length(x));
                 y1 = -1e9;
                 y2 = 1e9;
@@ -179,5 +283,9 @@ classdef comments < handle
         end
     end
     
+end
+
+function display_string = h__getDisplayString(string,I)
+display_string = sprintf(' %d) %s',I,string);
 end
 
